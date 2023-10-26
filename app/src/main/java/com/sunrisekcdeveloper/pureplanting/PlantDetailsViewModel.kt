5cb7b95f8242496b7900e8767a7e3cd829175f40
nadinecilliers <nadinebalanco@gmail.com>
2023-10-26 08:35:38 +0100

    Add :: PlantDetailsViewModel

---
+++ app/src/main/java/com/sunrisekcdeveloper/pureplanting/PlantDetailsViewModel.kt
@@ -0,0 +1,10 @@
+package com.sunrisekcdeveloper.pureplanting
+
+class PlantDetailsViewModel(
+    private val plant: Plant
+)
+
+/*
+* Not much to do here besides render the plant
+* I am keeping it here to handle navigation later on
+* */
