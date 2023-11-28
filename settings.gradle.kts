4ea4a0bccc281c01f42e34996c8b65b1590708a8
nadinecilliers <nadinebalanco@gmail.com>
2023-11-28 10:09:17 +0000

    Add :: domain and feature modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,3 +17,9 @@ dependencyResolutionManagement {
 rootProject.name = "PurePlanting"
 include(":app")
 include(":shared-test")
+include(":domain:plant")
+include(":domain:notification")
+include(":feature:plantList")
+include(":feature:plantDetail")
+include(":feature:addEditPlant")
+include(":feature:notificationList")
