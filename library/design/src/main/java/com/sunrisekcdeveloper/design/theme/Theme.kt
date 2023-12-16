2f2799c1083d7177b1cec9d657ec9ac8e0b188d8
nadinecilliers <nadinebalanco@gmail.com>
2023-12-16 19:03:09 +0000

    Add :: resources

--- library/design/src/main/java/com/sunrisekcdeveloper/design/theme/Theme.kt
+++ library/design/src/main/java/com/sunrisekcdeveloper/design/theme/Theme.kt
@@ -4,47 +4,43 @@ import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Typography
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
-import com.sunrisekcdeveloper.library.design.R
-import com.sunrisekcdeveloper.navigation.theme.Pink40
-import com.sunrisekcdeveloper.navigation.theme.Pink80
-import com.sunrisekcdeveloper.navigation.theme.Purple40
-import com.sunrisekcdeveloper.navigation.theme.Purple80
-import com.sunrisekcdeveloper.navigation.theme.PurpleGrey40
-import com.sunrisekcdeveloper.navigation.theme.PurpleGrey80
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    background = neutralus900,
+    onBackground = accent600,
+    secondary = accent100,
+    secondaryContainer = otherOlive100,
+    surface = neutralus0,
+    onSurface = neutralus100,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    background = neutralus0,
+    onBackground = otherOlive100,
+    secondary = accent600,
+    surface = neutralus300,
+    onSurface = neutralus900
 )
+
+/* Other default colors to override
+background = Color(0xFFFFFBFE),
+surface = Color(0xFFFFFBFE),
+onPrimary = Color.White,
+onSecondary = Color.White,
+onTertiary = Color.White,
+onBackground = Color(0xFF1C1B1F),
+onSurface = Color(0xFF1C1B1F),
+*/
 
 @Composable
 fun PurePlantingTheme(
